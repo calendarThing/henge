99b08538e10714f2b73502bc12d810a8831f8096
Tom McCluskey <bosun.tom@gmail.com>
2015-07-13 12:44:46 -0700

    User verification emails and related routes added, along with Redis

--- db/knexfile.js
+++ db/knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
       host     : process.env.APP_DB_HOST     || config.db_host,
       user     : process.env.APP_DB_USER     || config.db_user,
       password : process.env.APP_DB_PASSWORD || config.db_pass,
-      database : process.env.APP_DB_NAME     || 'henge'
+      database : process.env.APP_DB_NAME     || config.db_name
     },
     pool: {
       min: 2,
